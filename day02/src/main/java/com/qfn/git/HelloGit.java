ca4eb363995361a3fd82d6d574d9543da334eb3d
Taylove96 <2294694766@qq.com>
2021-02-17 20:02:31 +0800

    edit by develop

--- day02/src/main/java/com/qfn/git/HelloGit.java
+++ day02/src/main/java/com/qfn/git/HelloGit.java
@@ -7,5 +7,7 @@ package com.qfn.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println("edit by develop");
     }
 }
+
